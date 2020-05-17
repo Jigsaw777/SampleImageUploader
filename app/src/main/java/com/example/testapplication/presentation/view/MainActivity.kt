8ce0c4276a2df951ccbe7acad9d28b29909b3069
Sourik <sourik.ganguly@toppr.com>
2020-05-17 17:54:12 +0530

    First Commit

---
+++ app/src/main/java/com/example/testapplication/presentation/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.testapplication.presentation.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.testapplication.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
